fa166029d4ac1957a6cb45b2843c30a79088769b
Preng Biba <prengsen@galileo.edu>
2021-08-27 11:10:39 -0600

    Jefe ya agregue los cambios que me pidio para no despedirme. Tomar en cuenta que esta es la primera versión de los estilos, falta que el diseñador nos complete la paleta.

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ console.log('Git es Genial');
 
 console.log('Esto es una linea distinta');
 console.log('Esto es otra linea distinta');
+
+console.log('ESsto es otra linea de codigo en un dia nuevo');
+console.log('Jueguen The last of us 2');
